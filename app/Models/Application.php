1f02c2b7f2afddbb82281cb696981399293d62ff
Nubaysan <nubaysan@gmail.com>
2025-01-25 10:18:39 +0800

    feat: added applications management on filament

---
+++ app/Models/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Application extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = ['name', 'slug', 'description', 'user_id'];
+
+    public function uniqueIds()
+    {
+        return ['uuid'];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
