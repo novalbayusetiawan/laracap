59d68f6c1b7f5d1a3f85c4cc61afc6fab19b58df
Nubaysan <nubaysan@gmail.com>
2025-01-25 10:37:03 +0800

    feat: added delete action

--- app/Filament/Resources/ApplicationResource.php
+++ app/Filament/Resources/ApplicationResource.php
@@ -49,6 +49,7 @@ class ApplicationResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
