e2ee7a5ee964bb1e7ea1d330de5f2cf20e6ce5b4
Nubaysan <nubaysan@gmail.com>
2025-01-26 16:39:07 +0800

    feat: implement API endpoints for authentication and application bundles

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\AuthTokenController;
+use App\Http\Controllers\BundleController;
+use App\Http\Controllers\LatestAppBundleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('applications', [ApplicationController::class, 'index'])
+        ->name('applications.index');
+
+    Route::post('bundles', [BundleController::class, 'store'])
+        ->name('create-app-bundle');
+});
+
+
+Route::get('applications/{application}/bundles/latest', LatestAppBundleController::class)
+    ->name('latest-app-bundle');
+
+
+Route::post('login', [AuthTokenController::class, 'store'])
+    ->name('login');
+
+Route::delete('logout', [AuthTokenController::class, 'destroy'])
+    ->name('logout');
