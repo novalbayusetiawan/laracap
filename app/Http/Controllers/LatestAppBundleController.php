3ab345bdf1fc06869728e0ebce323b47c607c37c
Nubaysan <nubaysan@gmail.com>
2026-04-20 07:45:08 +0800

    feat: track device activity and bundle usage in latest bundle controllers

--- app/Http/Controllers/LatestAppBundleController.php
+++ app/Http/Controllers/LatestAppBundleController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\Device;
+use Illuminate\Http\Request;
 
 class LatestAppBundleController extends Controller
 {
-    public function __invoke(Application $application)
+    public function __invoke(Application $application, Request $request)
     {
+        $deviceIdentifier = $request->input('device_identifier') ?? $request->header('X-Device-Identifier');
+        $platform = $request->input('platform') ?? $request->header('X-Platform');
+        $bundleId = $request->input('bundle_id') ?? $request->header('X-Bundle-Id');
+
+        if ($deviceIdentifier && $platform) {
+            Device::updateOrCreate(
+                ['device_identifier' => $deviceIdentifier],
+                [
+                    'platform' => $platform,
+                    'bundle_id' => $bundleId,
+                    'last_active_at' => now(),
+                ]
+            );
+        }
+
         return response()->json($application->latestBundle);
     }
 }
--- app/Http/Controllers/LatestAppBundleDownloadController.php
+++ app/Http/Controllers/LatestAppBundleDownloadController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\Device;
+use Illuminate\Http\Request;
 
 class LatestAppBundleDownloadController extends Controller
 {
-    public function __invoke(Application $application)
+    public function __invoke(Application $application, Request $request)
     {
+        $deviceIdentifier = $request->input('device_identifier') ?? $request->header('X-Device-Identifier');
+        $platform = $request->input('platform') ?? $request->header('X-Platform');
+        // When downloading the latest, we might optionally imply they are on this version now or just update active time.
+        $bundleId = $request->input('bundle_id') ?? $request->header('X-Bundle-Id') ?? $application->latestBundle?->id;
+
+        if ($deviceIdentifier && $platform) {
+            Device::updateOrCreate(
+                ['device_identifier' => $deviceIdentifier],
+                [
+                    'platform' => $platform,
+                    'bundle_id' => $bundleId,
+                    'last_active_at' => now(),
+                ]
+            );
+        }
+
         return response()->download(storage_path('app/public/' . $application->latestBundle->file_path));
     }
 }
