28dbe3758f3beb732ce321b1c1f4f436575ebdda
Nubaysan <nubaysan@gmail.com>
2026-05-23 21:46:18 +0800

    feat: add application authorization and bundle limit checks in store method of BundleController

--- app/Http/Controllers/BundleController.php
+++ app/Http/Controllers/BundleController.php
@@ -29,7 +29,22 @@ class BundleController extends Controller
         $applicationId = $isUuid 
             ? Application::where('uuid', $request->application_id)->value('id') 
             : $request->application_id;
-            
+
+
+        $application = $request->user()->applications()->find($applicationId);
+
+        if (!$application) {
+            return response()->json([
+                'message' => 'You are not authorized to upload bundles for this application'
+            ], 403);
+        }
+
+        if ($application->bundles()->count() >= $application->bundle_limit) {
+            return response()->json([
+                'message' => 'You have reached the maximum number of bundles for this application'
+            ], 403);
+        }
+
         $bundleName = $request->name ?? $request->file('file')->getClientOriginalName();
 
         $channelId = Channel::where('name', $request->channel)->where('application_id', $applicationId)->value('id');
